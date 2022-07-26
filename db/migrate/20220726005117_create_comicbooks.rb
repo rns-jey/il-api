2761848297a8ca7db54d4c065e4c2c9671723b8e
JM Arenas <jm.arenas@interleukin.co>
2022-07-26 08:56:40 +0800

    Generated Comicbook model

---
+++ db/migrate/20220726005117_create_comicbooks.rb
@@ -0,0 +1,15 @@
+class CreateComicbooks < ActiveRecord::Migration[6.1]
+  def change
+    create_table :comicbooks do |t|
+      t.string :name
+      t.integer :number
+      t.string :artist
+      t.string :writer
+      t.string :image_url
+      t.string :description
+      t.integer :publisher_id
+
+      t.timestamps
+    end
+  end
+end
